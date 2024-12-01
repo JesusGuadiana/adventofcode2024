0c00362443f02d1e5db0fe3ce179556a9889fe40
Jesus Guadiana <jisus130@hotmail.com>
2024-12-01 12:36:06 -0600

    Day 1

---
+++ day1/day1.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	list1, list2 := readFile()
+
+	fmt.Println(getDistance(list1, list2))
+}
+
+func readFile() ([]float64, []float64) {
+	list1 := []float64{}
+	list2 := []float64{}
+
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return []float64{}, []float64{}
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		columns := strings.Fields(line)
+
+		if len(columns) >= 2 {
+			firstValue, _ := strconv.Atoi(columns[0])
+			secondValue, _ := strconv.Atoi(columns[1])
+
+			list1 = append(list1, float64(firstValue))
+			list2 = append(list2, float64(secondValue))
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+
+	return list1, list2
+}
+
+func getDistance(list1 []float64, list2 []float64) int {
+	sort.Float64s(list1)
+	sort.Float64s(list2)
+
+	totalDistance := 0.0
+
+	for i := 0; i < len(list2); i++ {
+		var difference float64 = list1[i] - list2[i]
+		totalDistance += math.Abs(difference)
+	}
+
+	return int(totalDistance)
+}
